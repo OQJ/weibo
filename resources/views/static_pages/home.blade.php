b3870f57f555ff24e8703b7e21e649f7915e552b
oqj <654570524@qq.com>
2019-11-21 13:09:53 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,6 +6,12 @@
 你现在所看到的是<a href="https://limitless-thicket-80157.herokuapp.com/" >laravel入门教程</a>的示例项目主页
 </p>
 <p>一切将从这里开始</p>
-<p><a class='btn btn-lg btn-success' herf='#' role='button'>现在注册</a></p>
+<p>
+    <a class='btn btn-lg btn-success' href="{{route('signup')}}" role='button'>
+            现在注册
+    </a>
+    
+</p>
+
 </div>
 @stop
