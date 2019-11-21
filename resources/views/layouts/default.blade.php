767aafda1659e8c141c53b952c811e05f61ebbeb
vagrant <vagrant@homestead>
2019-11-21 02:18:52 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title','weibo') - Laravel 新手入门教程</title>
+</head>
+<body>
+    <h1>@yield('content')</h1>
+    
+</body>
+</html>
